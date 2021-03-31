6be8a386e48be6fff1bca28706f9b4daf33120ed
June-Ruth <n-barral@hotmail.fr>
2021-03-31 16:50:14 +0200

    initial commit

---
+++ src/test/java/com/nnk/springboot/PasswordEncodeTest.java
@@ -0,0 +1,19 @@
+package com.nnk.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PasswordEncodeTest {
+
+    @Test
+    void testPassword() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String pw = encoder.encode("123456");
+        System.out.println("[ "+ pw + " ]");
+    }
+}
