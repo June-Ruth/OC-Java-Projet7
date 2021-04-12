2dcbb51664c581402e239688024614044d13a932
June-Ruth <n-barral@hotmail.fr>
2021-04-12 15:17:39 +0200

    controller + thymeleaf

--- src/main/java/com/nnk/springboot/controllers/UserController.java
+++ src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -1,9 +1,9 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.User;
-import com.nnk.springboot.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.nnk.springboot.services.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,64 +13,130 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
+    /**
+     * @see Logger
+     */
+    private static final Logger LOGGER =
+            LogManager.getLogger(UserController.class);
+    /**
+     * @see UserService
+     */
+    private UserService userService;
 
+    /**
+     * Autowired constructor.
+     * @param pUserService .
+     */
+    public UserController(final UserService pUserService) {
+        userService = pUserService;
+    }
+
+    /**
+     * Get all the user.
+     * @param model .
+     * @return all user.
+     */
     @RequestMapping("/user/list")
-    public String home(Model model)
-    {
-        model.addAttribute("users", userRepository.findAll());
+    public String home(final Model model) {
+        List<User> result = userService.findAllUser();
+        model.addAttribute("userList", result);
+        LOGGER.info("Find all user, size = " + result.size());
         return "user/list";
     }
 
+    /**
+     * Prepare the form to add a user.
+     * @param user .
+     * @return the form.
+     */
     @GetMapping("/user/add")
-    public String addUser(User bid) {
+    public String addUserForm(final User user) {
+        LOGGER.info("Show form to add user");
         return "user/add";
     }
 
+    /**
+     * Add a new user.
+     * @param user to add.
+     * @param result .
+     * @param model .
+     * @return the new list of user.
+     */
     @PostMapping("/user/validate")
-    public String validate(@Valid User user, BindingResult result, Model model) {
+    public String validate(@Valid final User user,
+                           final BindingResult result,
+                           final Model model) {
+        LOGGER.info("Try to save new user : " + user);
         if (!result.hasErrors()) {
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            user.setPassword(encoder.encode(user.getPassword()));
-            userRepository.save(user);
-            model.addAttribute("users", userRepository.findAll());
+            User user1 = userService.saveUser(user);
+            LOGGER.info("Save user : " + user1);
             return "redirect:/user/list";
         }
+        LOGGER.error("Can't save following user, "
+                + "must be invalid data :" + user);
         return "user/add";
     }
 
+    /**
+     * Get the form to update user.
+     * @param id of the user to update.
+     * @param model .
+     * @return form.
+     */
     @GetMapping("/user/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        user.setPassword("");
-        model.addAttribute("user", user);
+    public String showUpdateForm(@PathVariable("id") final Integer id,
+                                 final Model model) {
+        LOGGER.info("Show the form to update user with id " + id);
+        model.addAttribute("user", userService.findUserById(id));
         return "user/update";
     }
 
+    /**
+     * Update a user.
+     * @param id of the user to update.
+     * @param user with updated information.
+     * @param result .
+     * @param model .
+     * @return list with updated user.
+     */
     @PostMapping("/user/update/{id}")
-    public String updateUser(@PathVariable("id") Integer id, @Valid User user,
-                             BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "user/update";
+    public String updateUser(@PathVariable("id") final Integer id,
+                              @Valid final User user,
+                              final BindingResult result,
+                              final Model model) {
+        LOGGER.info("Try to update user with id " + id);
+        if (!result.hasErrors()) {
+            User user1 = userService.findUserById(id);
+            user1.setFullname(user.getFullname());
+            user1.setUsername(user.getUsername());
+            user1.setPassword(user.getPassword());
+            user1.setRole(user.getRole());
+            userService.saveUser(user1);
+            LOGGER.info("Success to update user " + user1);
+            return "redirect:/user/list";
         }
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        user.setPassword(encoder.encode(user.getPassword()));
         user.setId(id);
-        userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
-        return "redirect:/user/list";
+        LOGGER.error("Can't update following user, "
+                + "must be invalid data " + user);
+        return "user/update";
     }
 
+    /**
+     * Delete a user.
+     * @param id of user to delete.
+     * @param model .
+     * @return the new list of user.
+     */
     @GetMapping("/user/delete/{id}")
-    public String deleteUser(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        userRepository.delete(user);
-        model.addAttribute("users", userRepository.findAll());
+    public String deleteUser(@PathVariable("id") final Integer id,
+                              final Model model) {
+        LOGGER.info("Try to delete user with id : " + id);
+        User user = userService.findUserById(id);
+        userService.deleteUser(user);
         return "redirect:/user/list";
     }
 }
